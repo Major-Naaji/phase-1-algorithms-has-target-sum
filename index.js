e93f3335e5fbb1fc935816e5ae6e62bcf2617a0f
Zakaria Ali <najkacade100@gmail.com>
2022-06-19 22:13:34 +0300

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const numTarget=[];
+
+for(let num of array){
+  const numMatch=target-num;
+  if(numMatch in numTarget){
+    return true;
+  }
+  numTarget[num]=true;
+}
+
+return false;
 }
 
 /* 
